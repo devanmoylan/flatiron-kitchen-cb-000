47ead10747b305babfb61176c7aa9d9319abb43d
Devan <devan.moylan@backyardlion.com>
2017-07-25 16:37:52 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,29 @@
 class RecipesController < ApplicationController
+  before_action :recipe_params, only: [:create, :update]
+  before_action :set_recipe, only: [:show, :edit, :update]
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+  
+  def show
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name)
+  end
 end
